2ffa8f925afc0c99e9e02038a3c05ccdf47af780
Bim21 <chiennguyen2198@gmail.com>
2023-03-23 10:21:44 +0700

    User

--- job-portal/src/main/java/com/vti/entity/User.java
+++ job-portal/src/main/java/com/vti/entity/User.java
@@ -1,15 +1,44 @@
 package com.vti.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
+@Table(name = "`User`")
 public class User {
+
+    @Column(name = "id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(name = "username", length = 50, unique = true, nullable = false)
     private String username;
+
+    @Column(name = "`password`", length = 800, nullable = false)
     private String password;
+
+    @Column(name = "email", length = 200, nullable = false, unique = true)
     private String email;
+
+    @Column(name = "firstName", length = 50, nullable = false)
     private String firstName;
+
+    @Column(name = "lastName", length = 50, nullable = false)
     private String lastName;
+
+    @Column(name = "`role`")
+    @Enumerated(EnumType.STRING)
     private Role role;
+
+    @Column(name = "`status`", nullable = false)
+    @Enumerated(EnumType.STRING)
     private Status status;
 }
